886f0d78f3db70b70d2bfc023a7b7255a390aea6
jmatias-2019152 <jmatias-2019152@kinal.edu.gt>
2024-10-27 20:04:04 -0600

    Spring Postgres

---
+++ src/test/java/com/postgres/springPostgres/SpringPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postgres.springPostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
